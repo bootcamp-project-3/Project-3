236724564d3372353c5b935178c324e91210142d
John Doe <patrick.gould94@gmail.com>
2019-02-23 12:28:28 -0500

    Added new script

--- server.js
+++ server.js
@@ -4,26 +4,25 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
 
-
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+    app.use(express.static("client/build"));
+    
 
 // Connect app to mongo db
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/neighbor-app";
+    process.env.MONGODB_URI || "mongodb://localhost/neighbor-app";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Define API routes here
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// };
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+    console.log(`🌎 ==> API server now on port ${PORT}!`)   
+}
