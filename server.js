60ce1581fdc90806c1d8fe10579b994c34d3bfff
John Doe <patrick.gould94@gmail.com>
2019-02-26 18:26:48 -0500

    Added travis and new node modules

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
     
-
+};
 // Connect app to mongo db
 const MONGODB_URI =
     process.env.MONGODB_URI || "mongodb://localhost/neighbor-app";
@@ -21,7 +21,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-};
+}
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`)   
