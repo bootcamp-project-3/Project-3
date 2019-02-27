e3e949b3570366e7452131a415a709e6ee755c94
John Doe <patrick.gould94@gmail.com>
2019-02-26 20:22:28 -0500

    added react router in app.js and signin and nomatch

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class NoMatch extends Component {
+  render() {
+    return(
+      <div>
+        <h1>404</h1>
+        <h2>Sorry We Couldn't Find That Page.</h2>
+      </div>
+    )
+  }
+}
+
+export default NoMatch;
---
+++ client/src/pages/SignIn.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SignIn extends Component {
+    render() {
+      return(
+        <div>
+
+        </div>
+      )
+    }
+}
+
+export default SignIn;
