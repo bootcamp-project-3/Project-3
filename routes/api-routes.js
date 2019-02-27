b19418f0d304dec4383cb6b22278266b4c3a8e39
Ordolph <austin.t.randolph@gmail.com>
2019-02-27 14:10:53 -0500

    Added a post api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,14 +4,15 @@ const bcrypt = require("bcrypt");
 
 module.exports = function(app) {
     // TODO: Add security layer to authorize post request, perhaps captcha?
+    // * Adds new user to db
     app.post("/api/users", function(req, res) {
         // Searches db for input email
-        User.findOne({ email: req.body.email }, function(err, result) {
+        User.findOne({ email: req.body.email }, function(err, user) {
             if (err) {
                 console.log(err);
             } else {
                 // If no duplicates present
-                if (result === null) {
+                if (user === null) {
                     // Hash password
                     bcrypt.hash(req.body.password, 10, function(err, hash) {
                         if (err) {
@@ -34,6 +35,16 @@ module.exports = function(app) {
                     // TODO: Add functionality to send a response to the frontend if duplicate is present
                     res.end();
                 }
+            }
+        });
+    });
+    // * Adds new post to db
+    app.post("/api/posts", function(req, res) {
+        Post.create(req.body, function(err, post) {
+            if (err){
+                console.log(err);
+            } else {
+                console.log(post);
             }
         });
     });
