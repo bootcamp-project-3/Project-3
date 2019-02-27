201074f04e831b19cc91239e09631dbbddebe6f9
mattylaw192 <mattylaw192@gmail.com>
2019-02-26 21:06:33 -0500

    Updated homepage.js by adding nav test.

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import LPNav from "../components/LPNav";
 
 class HomePage extends Component {
     render() {
       return(
         <div>
-
+            <LPNav
+            title="Around The Corner"
+            />
         </div>
       )
     }
