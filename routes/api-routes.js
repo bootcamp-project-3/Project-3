5d2bdefa3e28d962de46652df17c520dc938b0e1
Ordolph <austin.t.randolph@gmail.com>
2019-02-27 13:51:10 -0500

    Added password hashing to user creation and some comments

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,23 +3,35 @@ const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 module.exports = function(app) {
+    // TODO: Add security layer to authorize post request, perhaps captcha?
     app.post("/api/users", function(req, res) {
-        console.log("sending");
+        // Searches db for input email
         User.findOne({ email: req.body.email }, function(err, result) {
             if (err) {
                 console.log(err);
             } else {
-                res.end();
+                // If no duplicates present
                 if (result === null) {
-                    User.create(req.body, function(err, post) {
+                    // Hash password
+                    bcrypt.hash(req.body.password, 10, function(err, hash) {
                         if (err) {
                             console.log(err);
                         } else {
-                            console.log(post);
+                            req.body.password = hash;
+                            // Then add to db
+                            User.create(req.body, function(err, post) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    console.log(post);
+                                }
+                                res.end();
+                            });
                         }
-                        res.end();
                     });
+                    // Otherwise escape
                 } else {
+                    // TODO: Add functionality to send a response to the frontend if duplicate is present
                     res.end();
                 }
             }
