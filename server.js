84119e5c3faa47f89da2c516931f9923d6c1aed1
John Doe <patrick.gould94@gmail.com>
2019-02-27 19:10:52 -0500

    test

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
+// Test branch
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
