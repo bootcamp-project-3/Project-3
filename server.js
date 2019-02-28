2c61d5fa6fae47a54c3565b21ae1191f04ef2099
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2019-02-28 18:17:38 -0500

    dotenv added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
@@ -6,15 +7,13 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-let clientID =
-    process.env.ClientID ||
-    "898849334215-vqdder0i78oq04vs8aovtpjpai5e3uqh.apps.googleusercontent.com";
+let clientID = process.env.googleClientID;
 
-let clientSecret = process.env.ClientSecret || "oi5oPte2fD_q9AxcTmGc9CFq";
+let clientSecret = process.env.googleClientSecret;
 
 // Define middleware here
 app.use(
-    express.urlencoded({
+  express.urlencoded({
     extended: true,
     })
 );
@@ -56,9 +55,9 @@ app.get(
 
 app.get("/auth/google/callback", passport.authenticate("google"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 
 app.listen(PORT, () => {
