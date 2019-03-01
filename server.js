7664579038b3dd0d0ed9a7b685d453a4d41ef2fd
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2019-02-28 20:52:29 -0500

    fixing refactor

--- server.js
+++ server.js
@@ -1,47 +1,66 @@
 require('dotenv').config();
 const express = require("express");
-
-require("./services/passport");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 const app = express();
 const mongoose = require("mongoose");
-
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 let clientID = process.env.ClientID;
 
 let clientSecret = process.env.ClientSecret;
 
-
-
 // Define middleware here
 app.use(
   express.urlencoded({
     extended: true,
-    })
+  })
 );
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
 // Connect app to mongo db
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/neighbor-app";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+    process.env.MONGODB_URI || "mongodb://localhost/neighbor-app";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true
+});
 
 // Define API routes here
 require("./routes/api-routes.js")(app);
 
 
+passport.use(
+    new GoogleStrategy({
+            clientID: clientID,
+            clientSecret: clientSecret,
+            callbackURL: "/auth/google/callback",
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log("access token", accessToken);
+            console.log("refresh token", refreshToken);
+            console.log("profile:", profile);
+        }
+    )
+);
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"],
+    })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
 app.get("*", (req, res) => {
-   res.sendFile(path.join(__dirname, "./client/build/index.html"));
- });
-
-require("./routes/authRoutes")(app);
-
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
-   console.log(`🌎 ==> API server now on port ${PORT}!`)
+    console.log(`🌎 ==> API server now on port ${PORT}!`)
 });
