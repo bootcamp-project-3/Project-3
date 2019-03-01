239efe6f296f27b026836dbd24ca04bf1a238b2c
John Doe <patrick.gould94@gmail.com>
2019-02-28 20:22:16 -0500

    Added stuff

--- client/src/components/Nav/SideBar/SideBar.js
+++ client/src/components/Nav/SideBar/SideBar.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
