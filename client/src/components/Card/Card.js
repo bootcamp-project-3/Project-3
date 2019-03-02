e6bc2c9b785dcd424a61bfc378b63decbae3d6fb
John Doe <patrick.gould94@gmail.com>
2019-03-01 19:17:58 -0500

    Fixed another linting error

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Styled from "styled-components";
 import { withStyles } from "@material-ui/core/styles";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
