313b7aa46d8f673e41b65253768c231a0574c50a
John Doe <patrick.gould94@gmail.com>
2019-03-05 19:39:43 -0500

    Added stuff for dinah

--- client/src/pages/Bulletin.js
+++ client/src/pages/Bulletin.js
@@ -2,11 +2,21 @@ import React, { Component } from "react";
 import SideBar from "../components/Nav/SideBar/SideBar";
 import Panel from "../components/Card/Panel";
 import Styled from "styled-components";
-import BaseCard from "../components/EventCard/BaseCard";
+import BaseEventCard from "../components/EventCard/BaseEventCard";
+import BaseGeneralCard from "../components/GeneralCard/BaseGeneralCard";
 
 const SmallWrapperDiv = Styled.div`
   display: grid;
   grid-template-columns: repeat(3,1fr);
+  justify-items: center;
+  align-items: center;
+  max-width: 85%;
+  margin: 30px auto;
+`;
+
+const MediumWrapperDiv = Styled.div`
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
   justify-items: center;
   align-items: center;
   max-width: 85%;
@@ -50,36 +60,36 @@ class Bulletin extends Component {
           this.updatePosts(result);
         },
         error => {
-          console.log(error)
+          console.log(error);
         }
       );
 
-      fetch("/api/session", {
-        method: "Get", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, cors, *same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "include", // include, *same-origin, omit
-        headers: {
-          "Content-Type": "application/json",
-          // "Content-Type": "application/x-www-form-urlencoded",
+    fetch("/api/session", {
+      method: "Get", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, cors, *same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "include", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // "Content-Type": "application/x-www-form-urlencoded",
+      },
+      redirect: "follow", // manual, *follow, error
+      referrer: "client", // no-referrer, *client
+    })
+      .then(res => res.json())
+      .then(
+        result => {
+          console.log(result);
+          console.log(result.data);
+          this.setState({
+            id: result.data.user,
+            location: result.data.loc,
+          });
         },
-        redirect: "follow", // manual, *follow, error
-        referrer: "client", // no-referrer, *client
-      })
-        .then(res => res.json())
-        .then(
-          result => {
-            console.log(result);
-            console.log(result.data)
-            this.setState({
-              id: result.data.user,
-              location: result.data.loc
-            });
-          },
-          error => {
-            console.log(error)
-          }
-        );
+        error => {
+          console.log(error);
+        }
+      );
   }
 
   updatePosts = posts => {
@@ -98,14 +108,21 @@ class Bulletin extends Component {
             posts={this.state.posts}
             updatePosts={this.updatePosts}
           />
-          </SmallWrapperDiv>
-          <LargeWrapperDiv>
-          <BaseCard 
-            category="Community Events"
+        </SmallWrapperDiv>
+        <LargeWrapperDiv>
+          <BaseGeneralCard
+            category="General"
             posts={this.state.posts}
             updatePosts={this.updatePosts}
           />
         </LargeWrapperDiv>
+        <MediumWrapperDiv>
+          <BaseEventCard
+            category="Community Events"
+            posts={this.state.posts}
+            updatePosts={this.updatePosts}
+          />
+        </MediumWrapperDiv>
       </main>
     );
   }
