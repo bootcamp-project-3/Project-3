59333e2d3787a5fd7e17d37f95a637ab873a7165
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2019-03-06 13:55:56 -0500

    improved dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,24 +1,107 @@
 import React, { Component } from "react";
 import SideBar from "../components/Nav/SideBar/SideBar";
 import Styled from "styled-components";
+import Card from "@material-ui/core/Card";
+import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button"
+import EditIcon from "@material-ui/icons/Edit"
+
+
 
 const ContainerDiv = Styled.div`
 display: grid;
-grid-template:;
+grid-template-columns: 1fr 1fr;
+grid-gap: 15px;
+width: 50%;
+margin: auto
 `;
-class Dashboard extends Component {
-    state = {
 
-    }
+    
+    class Dashboard extends Component {
 
+      state = {
+        name: "",
+        id: "",
+        location: "",
+        nameDisabled: false,
+        locationDisabled: true,
+      }
+
+        componentDidMount() {
+        fetch("/api/session", {
+            method: "Get", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "include", // include, *same-origin, omit
+            headers: {
+              "Content-Type": "application/json",
+              // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            redirect: "follow", // manual, *follow, error
+            referrer: "client", // no-referrer, *client
+          })
+          .then(res =>res.json())
+          .then(
+              result => {
+                console.log(result.data)
+                this.setState({
+                  id: result.data.user,
+                  location: result.data.loc,
+                  name: result.data.name,
+                });
+              },
+              error => {
+                console.log(error)
+              }
+          )
+            }
+
+            handleChange=event=>{
+              const {name, value} = event.target;
+
+              
+              this.setState({
+                [name]: value
+              })
+            }
+
+            handleClick = name => {
+              this.setState({
+                [name]: !this.state[name],
+              })
+              console.log(this.state[name])
+            }
+        
+    
     render() {
+
         return(
-            <ContainerDiv>
+            <div>
             <SideBar />
-            <main>
-            <p>Test</p>
-            </main>
+            <ContainerDiv>
+              <Card>
+                <TextField
+                value={this.state.name}
+                 disabled={this.state.nameDisabled}
+                 placeholder={this.state.name}>{this.state.name}
+                 
+                </TextField>
+                <Button onClick={() => this.handleClick("nameDisabled")}>
+                   <EditIcon />
+                 </Button>
+              </Card>
+              <Card>
+                <TextField
+                 disabled={this.state.locationDisabled}
+                 placeholder={this.state.location}
+                 value={this.state.location}>
+                </TextField> 
+                <Button onClick={() => this.handleClick("locationDisabled")}>
+                   <EditIcon />
+                 </Button>
+              </Card>
             </ContainerDiv>
+            </div>
         )
     }
 }
