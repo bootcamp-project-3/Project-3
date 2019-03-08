fcff9d097a66980bb8cfa5cc440e8d3f0432186f
mattylaw192 <mattylaw192@gmail.com>
2019-03-07 19:04:53 -0500

    Fixed issues

--- client/src/components/LandingPage/LpNav.js
+++ client/src/components/LandingPage/LpNav.js
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
+// import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import MenuIcon from '@material-ui/icons/Menu';
+// import MenuIcon from '@material-ui/icons/Menu';
 import Image from "../LandingPage/assets/neighborlyFinal.png";
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 
 
 
