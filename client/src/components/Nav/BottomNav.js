bc04983f2ea239bd52cc277f852875e2393ea3eb
mattylaw192 <mattylaw192@gmail.com>
2019-03-09 12:13:35 -0500

    Updated links

--- client/src/components/Nav/BottomNav.js
+++ client/src/components/Nav/BottomNav.js
@@ -8,46 +8,50 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 const styles = {
-  root: {
-    width: "100%",
-  }
+    root: {
+        width: "100%",
+    }
 };
 
 
 class BottomNav extends React.Component {
-  state = {
-    value: 0,
-  };
+    state = {
+        value: 0,
+    };
 
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
+    handleChange = (event, value) => {
+        this.setState({ value });
+    };
 
-  render() {
-    const { classes } = this.props;
-    const { value } = this.state;
+    render() {
+        const { classes } = this.props;
+        const { value } = this.state;
 
-    return (
-      <BottomNavigation
-        value={value}
-        onChange={this.handleChange}
-        showLabels
-        className={classes.bottomNav}
-   
-        
-        
+        return (
+            <BottomNavigation
+                value={value}
+                onChange={this.handleChange}
+                showLabels
+                className={classes.bottomNav}
 
-      >
-        <BottomNavigationAction label="Github" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Privacy Policy" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Contact Us" icon={<LocationOnIcon />} />
-      </BottomNavigation>
-    );
-  }
+
+
+
+            >
+                <BottomNavigationAction label="Github" icon={<RestoreIcon />}onClick={() => {
+                    window.open("https://github.com/bootcamp-project-3/Project-3" , '_blank');
+                }} /> />
+                <BottomNavigationAction label="Terms of Service" icon={<FavoriteIcon />} onClick={() => {
+                    window.location = "/TermsOfService";
+                }} />
+                <BottomNavigationAction label="Contact Us" icon={<LocationOnIcon />} onClick={ () => {window.location.href = "mailto:user@example.com?subject=Neighborly&body=message%20goes%20here";}} />
+            </BottomNavigation>
+        );
+    }
 }
 
 BottomNav.propTypes = {
-  classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(BottomNav);
