607b51d7d24f4910680aaccf766a70c0ad5d6cb2
John Doe <patrick.gould94@gmail.com>
2019-03-11 00:05:30 -0400

    Added new page links and react router to sidebar

---
+++ client/src/components/Nav/SideBar/Routes.js
@@ -0,0 +1,29 @@
+import { Home, Inbox, Mail, AccountBox, Event } from "@material-ui/icons";
+import Bulletin from "../../../pages/Bulletin";
+import InboxPage from "../../../pages/InboxPage";
+
+const Routes = [
+  {
+    path: "/bulletin",
+    sidebarName: "Home",
+    navbarName: "Home",
+    icon: Home,
+    component: Bulletin,
+  },
+  {
+    path: "/inbox",
+    sidebarName: "Inbox",
+    navbarName: "Inbox",
+    icon: Inbox,
+    component: InboxPage,
+  },
+  {
+    path: "/profile",
+    sidebarName: "Profile",
+    navbarName: "Profile",
+    icon: AccountBox,
+    component: InboxPage,
+  },
+];
+
+export default Routes;
