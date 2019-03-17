ee8c0ad579aa041eec827e0dba22574206af996c
John Doe <patrick.gould94@gmail.com>
2019-03-17 04:11:22 -0400

    Fixed linting issue

--- client/src/components/SnackBar/SnackBar.js
+++ client/src/components/SnackBar/SnackBar.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Snackbar from '@material-ui/core/Snackbar';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 
 const styles = theme => ({
   close: {
@@ -19,14 +18,14 @@ class SimpleSnackbar extends React.Component {
       <div>
         <Snackbar
           anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
+            vertical: "bottom",
+            horizontal: "left",
           }}
           open={this.props.open}
           autoHideDuration={6000}
           onClose={this.props.closeSnackBar}
           ContentProps={{
-            'aria-describedby': 'message-id',
+            "aria-describedby": "message-id",
           }}
           message={<span id="message-id">Message Sent</span>}
           action={[
